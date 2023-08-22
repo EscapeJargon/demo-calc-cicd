3c806d69dd811966b64346c0ef2876e048e72875
EscapeJargon <romellom@hotmail.com>
2023-08-21 21:58:58 -0400

    TC

--- src/__tests__/unit_tests/calculate.unit.test.js
+++ src/__tests__/unit_tests/calculate.unit.test.js
@@ -1,19 +1,24 @@
 // add one jest testr
 
 import { calculate } from "../../App";
-
+//1
 test("adds two numbers correctly", () => {
   expect(calculate(3, "+", 5)).toBe(8);
 });
-
+//2
 test("subtracts two numbers correctly", () => {
   expect(calculate(8, "-", 3)).toBe(5);
 });
-
+//3
 test('returns "Error" for invalid operator', () => {
   expect(calculate(5, "*", 2)).toBe("Error");
 });
-
+//4
 test('returns "Error" for invalid operands', () => {
   expect(calculate("abc", "+", 2)).toBe("Error");
+});
+///5
+//added
+test('returns "Error" when no text fields are filled', () => {
+  expect(calculate("+")).toBe("Error");
 });
--- src/__tests__/unit_tests/calculator.unit.test.js
+++ src/__tests__/unit_tests/calculator.unit.test.js
@@ -2,13 +2,13 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import App from "../../App";
-
+//1
 test("renders calculator title", () => {
   render(<App />);
   const titleElement = screen.getByText(/Simple Calculator/i);
   expect(titleElement).toBeInTheDocument();
 });
-
+//2
 test("performs addition correctly", () => {
   render(<App />);
   const num1Input = screen.getByLabelText("Number 1");
@@ -22,5 +22,22 @@ test("performs addition correctly", () => {
   fireEvent.click(calculateButton);
 
   const resultElement = screen.getByText("Result: 8");
+  expect(resultElement).toBeInTheDocument();
+});
+//3
+//added
+test("performs substraction correctly", () => {
+  render(<App />);
+  const num1Input = screen.getByLabelText("Number 1");
+  const operatorSelect = screen.getByLabelText("Operator");
+  const num2Input = screen.getByLabelText("Number 2");
+  const calculateButton = screen.getByText("Calculate");
+
+  fireEvent.change(num1Input, { target: { value: "50" } });
+  fireEvent.change(operatorSelect, { target: { value: "-" } });
+  fireEvent.change(num2Input, { target: { value: "50" } });
+  fireEvent.click(calculateButton);
+
+  const resultElement = screen.getByText("Result: 0");
   expect(resultElement).toBeInTheDocument();
 });
